c0ad15ab206b2e7a21ed49810220454c49d874d9
AhmedYousof <31749142+AhmedYousof@users.noreply.github.com>
2018-05-26 11:28:18 +0200

    first Version

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const {generateMessage, generateLocationMessage} = require('./utils/message');
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to The Chat app'));
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'new user joined'));
+
+  socket.on('createMessage', (message, callback) => {
+    console.log('create message', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback();
+  });
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude ,coords.longitude));
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
---
+++ server/utils/message.test.js
@@ -0,0 +1,31 @@
+const expect = require('expect');
+
+var {generateMessage, generateLocationMessage} = require('./message');
+
+describe('generateMessage', () => {
+    it('Should generate correct message object', () => {
+        var from = 'a1';
+        var text = 'some message';
+        var message = generateMessage(from, text);
+
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({
+            from, 
+            text
+        });
+
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('Should generate correct location object', () => {
+        var from = 'Dep';
+        var latitude = 15;
+        var longitude = 19;
+        var url = 'https://www.google.com/maps?q=15,19';
+        var message = generateLocationMessage(from, latitude, longitude);
+
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({from,url});
+    });
+});
